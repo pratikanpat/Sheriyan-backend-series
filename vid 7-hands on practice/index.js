87c305a830006020caa850cc6ce71cb2e696321b
Pratik <pratikanpat89@gmail.com>
2025-11-23 20:04:30 +0530

    notes project

--- vid 7-hands on practice/index.js
+++ vid 7-hands on practice/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const fs = require('fs');
 
 app.set('view engine', "ejs");
 app.use(express.json());
@@ -9,8 +10,25 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    res.render("index");
+    fs.readdir(`./files`, function(err, files){
+    res.render("index", {files:files});
+    })
 });
+
+app.post('/create', function(req, res){
+    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.details, function(err){
+        res.redirect("/")
+    })
+});
+
+app.get('/file/:filename', function(req, res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+        res.render('show', {filename:req.params.filename, filedata: filedata});
+    })
+});
+
+
+
 
 app.listen(port, function(){
     console.log(`server runing on http://localhost:${port}`);
